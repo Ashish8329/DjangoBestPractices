d7a2e53a4fe63f6e696ad462cee60eb9fa2b20ee
Ashish Auti <ashishauti123@gmail.com>
2025-03-05 19:12:44 +0530

    chore : add user model

--- baseProject/baseApp/models.py
+++ baseProject/baseApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from base.base_models import BaseModel
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(BaseModel):
+    username = models.CharField(
+        max_length=100,
+        verbose_name=_("Username"), 
+        null=True, #db nullability
+        blank=True, # allow null values
+        help_text=_("This is the user's name.") 
+    )
+
+    class Meta:
+        verbose_name = _("User")    
+        verbose_name_plural = _("Users")
+    
+    def __str__(self):
+        return self.username or _("Unnamed User")
