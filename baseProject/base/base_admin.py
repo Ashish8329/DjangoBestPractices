03bd74dc476b3e114da12a879bd661215d171dcd
Ashish Auti <ashishauti123@gmail.com>
2025-03-26 19:28:24 +0530

    refactor : configure base files

--- baseProject/base/base_admin.py
+++ baseProject/base/base_admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 
 class BaseAdmin(admin.ModelAdmin):
-    ordering = ("-created_at",)  # - for decending newest first
+    ordering = ("-created_at",)
     readonly_fields = ("created_at", "updated_at")
