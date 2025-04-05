47a5a8b7ce100bb072e39a44b568c0bb4e378e80
Ashish Auti <ashishauti123@gmail.com>
2025-04-05 13:46:55 +0530

    feat : update url path

--- baseProject/baseProject/urls.py
+++ baseProject/baseProject/urls.py
@@ -20,7 +20,22 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api", include("baseApp.urls"))]
+urlpatterns = [
+
+    path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include(
+            [
+                path(
+                    "baseApp/",
+                    include("baseApp.urls"),
+                ),
+            ]
+        ),
+    ),
+]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
